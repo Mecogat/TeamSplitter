b4b9a9b84e1dcd7356c87b04835e4cc7f937fbb6
Mecogat <140114929+Mecogat@users.noreply.github.com>
2023-08-09 07:50:06 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -7,166 +7,118 @@ Version: 5.5.0
 """
 
 import random
-
+import Maps
+import Scoreupdate
+import FN
+import generateorder
+import generateteam
+import noequalzero
 import aiohttp
 import discord
+from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
 
 from helpers import checks
 
 
-class Choice(discord.ui.View):
+class ResultView(discord.ui.View):
     def __init__(self):
         super().__init__()
-        self.value = None
+        self.value = 0
 
-    @discord.ui.button(label="Heads", style=discord.ButtonStyle.blurple)
-    async def confirm(
-        self, button: discord.ui.Button, interaction: discord.Interaction
-    ):
-        self.value = "heads"
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label='Team 1', style=discord.ButtonStyle.green)
+    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = 1
         self.stop()
 
-    @discord.ui.button(label="Tails", style=discord.ButtonStyle.blurple)
-    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
-        self.value = "tails"
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label='Team 2', style=discord.ButtonStyle.grey)
+    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = 2
         self.stop()
-
-
-class RockPaperScissors(discord.ui.Select):
-    def __init__(self):
-        options = [
-            discord.SelectOption(
-                label="Scissors", description="You choose scissors.", emoji="✂"
-            ),
-            discord.SelectOption(
-                label="Rock", description="You choose rock.", emoji="🪨"
-            ),
-            discord.SelectOption(
-                label="paper", description="You choose paper.", emoji="🧻"
-            ),
-        ]
-        super().__init__(
-            placeholder="Choose...",
-            min_values=1,
-            max_values=1,
-            options=options,
-        )
-
-    async def callback(self, interaction: discord.Interaction):
-        choices = {
-            "rock": 0,
-            "paper": 1,
-            "scissors": 2,
-        }
-        user_choice = self.values[0].lower()
-        user_choice_index = choices[user_choice]
-
-        bot_choice = random.choice(list(choices.keys()))
-        bot_choice_index = choices[bot_choice]
-
-        result_embed = discord.Embed(color=0x9C84EF)
-        result_embed.set_author(
-            name=interaction.user.name, icon_url=interaction.user.avatar.url
-        )
-
-        if user_choice_index == bot_choice_index:
-            result_embed.description = f"**That's a draw!**\nYou've chosen {user_choice} and I've chosen {bot_choice}."
-            result_embed.colour = 0xF59E42
-        elif user_choice_index == 0 and bot_choice_index == 2:
-            result_embed.description = f"**You won!**\nYou've chosen {user_choice} and I've chosen {bot_choice}."
-            result_embed.colour = 0x9C84EF
-        elif user_choice_index == 1 and bot_choice_index == 0:
-            result_embed.description = f"**You won!**\nYou've chosen {user_choice} and I've chosen {bot_choice}."
-            result_embed.colour = 0x9C84EF
-        elif user_choice_index == 2 and bot_choice_index == 1:
-            result_embed.description = f"**You won!**\nYou've chosen {user_choice} and I've chosen {bot_choice}."
-            result_embed.colour = 0x9C84EF
-        else:
-            result_embed.description = (
-                f"**I won!**\nYou've chosen {user_choice} and I've chosen {bot_choice}."
-            )
-            result_embed.colour = 0xE02B2B
-        await interaction.response.edit_message(
-            embed=result_embed, content=None, view=None
-        )
-
-
-class RockPaperScissorsView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.add_item(RockPaperScissors())
-
+    
+    @discord.ui.button(label='end scrim', style=discord.ButtonStyle.blurple)
+    async def blue(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.value = 3
+        self.stop()
 
 class Fun(commands.Cog, name="fun"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command(name="randomfact", description="Get a random fact.")
+    @commands.hybrid_command(
+        name="start", description="Play Teamsplitter scrim by stating the 8 players names.",
+    )
     @checks.not_blacklisted()
-    async def randomfact(self, context: Context) -> None:
-        """
-        Get a random fact.
+    @app_commands.describe(
+        p1="First player on team 1",
+        p2="Second player on team 1",
+        p3="Third player on team 1",
+        p4="Fourth player on team 1",
+        p5="First player on team 2",
+        p6="Second player on team 2",
+        p7="Third player on team 2",
+        p8="Fourth player on team 2",
+    )
 
-        :param context: The hybrid command context.
-        """
-        # This will prevent your bot from stopping everything when doing a web request - see: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-make-a-web-request
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                "https://uselessfacts.jsph.pl/random.json?language=en"
-            ) as request:
-                if request.status == 200:
-                    data = await request.json()
-                    embed = discord.Embed(description=data["text"], color=0xD75BF4)
+    async def start(self, context: Context, p1: discord.User, p2: discord.User, p3: discord.User, p4: discord.User, p5: discord.User, p6: discord.User, p7: discord.User, p8: discord.User) -> None:
+        player1 = context.guild.get_member(p1.id) or await context.guild.fetch_member(p1.id)
+        player2 = context.guild.get_member(p2.id) or await context.guild.fetch_member(p2.id)
+        player3 = context.guild.get_member(p3.id) or await context.guild.fetch_member(p3.id)
+        player4 = context.guild.get_member(p4.id) or await context.guild.fetch_member(p4.id)
+        player5 = context.guild.get_member(p5.id) or await context.guild.fetch_member(p5.id)
+        player6 = context.guild.get_member(p6.id) or await context.guild.fetch_member(p6.id)
+        player7 = context.guild.get_member(p7.id) or await context.guild.fetch_member(p7.id)
+        player8 = context.guild.get_member(p8.id) or await context.guild.fetch_member(p8.id)
+        players = [player1, player2, player3, player4, player5, player6, player7, player8]
+        scores = [0] * 8
+        win = [0] * 8
+        lose = [0] * 8
+        team1 = [0, 1, 2, 3]
+        team2 = [4, 5, 6, 7]
+        ss = [0, 1, 2, 3, 4, 5, 6, 7]
+        bufteams = [0] * 8
+        teams = players
+        y = 0
+        wt = 0
+        counter = 1
+        while(y == 0):
+            map = Maps.Randommap(counter)
+            counter += 1
+            x = 0
+            while(x == 0):
+                view = ResultView()
+                await context.send(f"{map} \nTeam 1 is **{teams[0]}** **{teams[1]}** **{teams[2]}** **{teams[3]}** \n Team 2 is **{teams[4]}** **{teams[5]}** **{teams[6]}** **{teams[7]}** \nPlease fill in which team won", view=view, delete_after=180.0)
+                await view.wait()
+                if(view.value == 0):
+                    await context.send('Timed out', delete_after=3.0)
+                elif(view.value == 1):
+                    wt = 1
+                    x = 1
+                elif(view.value == 2):
+                    wt = 2
+                    x = 1
                 else:
-                    embed = discord.Embed(
-                        title="Error!",
-                        description="There is something wrong with the API, please try again later",
-                        color=0xE02B2B,
-                    )
-                await context.send(embed=embed)
-
-    @commands.hybrid_command(
-        name="coinflip", description="Make a coin flip, but give your bet before."
-    )
-    @checks.not_blacklisted()
-    async def coinflip(self, context: Context) -> None:
-        """
-        Make a coin flip, but give your bet before.
-
-        :param context: The hybrid command context.
-        """
-        buttons = Choice()
-        embed = discord.Embed(description="What is your bet?", color=0x9C84EF)
-        message = await context.send(embed=embed, view=buttons)
-        await buttons.wait()  # We wait for the user to click a button.
-        result = random.choice(["heads", "tails"])
-        if buttons.value == result:
-            embed = discord.Embed(
-                description=f"Correct! You guessed `{buttons.value}` and I flipped the coin to `{result}`.",
-                color=0x9C84EF,
-            )
-        else:
-            embed = discord.Embed(
-                description=f"Woops! You guessed `{buttons.value}` and I flipped the coin to `{result}`, better luck next time!",
-                color=0xE02B2B,
-            )
-        await message.edit(embed=embed, view=None, content=None)
-
-    @commands.hybrid_command(
-        name="rps", description="Play the rock paper scissors game against the bot."
-    )
-    @checks.not_blacklisted()
-    async def rock_paper_scissors(self, context: Context) -> None:
-        """
-        Play the rock paper scissors game against the bot.
-
-        :param context: The hybrid command context.
-        """
-        view = RockPaperScissorsView()
-        await context.send("Please make your choice", view=view)
-
+                    y = 1
+                    x = 1
+            if(y == 1):
+                y = 1
+            else:
+                scores = Scoreupdate.scoreupdt(scores, wt, ss)
+                await context.send(f"{players[0]}: {scores[0]} \n{players[1]}: {scores[1]} \n{players[2]}: {scores[2]} \n{players[3]}: {scores[3]} \n{players[4]}: {scores[4]} \n{players[5]}: {scores[5]} \n{players[6]}: {scores[6]} \n{players[7]}: {scores[7]}")
+                ah = FN.FNN(scores, players)
+                if not ah:
+                    re = noequalzero.FN(scores)
+                else:
+                    lre = len(ah)
+                    randomIndex = random.randint(0,lre)
+                    re = ah[randomIndex]
+                teams = generateteam.gt(scores, players, re)
+                ss = generateorder.gso(scores, players, re)
 
 async def setup(bot):
     await bot.add_cog(Fun(bot))
